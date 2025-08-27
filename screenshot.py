c7de3c88e33e69fee507fa975f58a96d04739f22
Matheus Ferreira <mxthferreira@gmail.com>
2025-08-26 21:47:17 -0300

    Adiciona workflow de update automático

--- screenshot.py
+++ screenshot.py
@@ -1,24 +1,24 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import chromedriver_autoinstaller
+import time
 
 URL = "https://www.webcup.com.br/campeonato/campeonato-de-cf-global-2025---gta-torcidas-1754250411"
 
 def main():
-    chromedriver_autoinstaller.install()
-
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless")  # roda sem abrir janela
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=options)
-    driver.get(URL)
-    time.sleep(5)  # espera carregar
+    driver.set_window_size(1920, 1080)  # resolução grande
 
-    tabela = driver.find_element("tag name", "table")
-    tabela.screenshot("public/ranking.png")
+    print("🔄 Acessando o Webcup...")
+    driver.get(URL)
+    time.sleep(5)  # espera a página carregar (pode aumentar se ficar em branco)
+
+    driver.save_screenshot("ranking.png")
+    print("✅ Screenshot completo salvo como ranking.png")
 
     driver.quit()
 
